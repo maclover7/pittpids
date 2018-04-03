9c8b75f2071e31be7e482e34dee2581fb1bc26e5
Jon Moss <me@jonathanmoss.me>
2018-04-02 20:07:25 -0400

    Initial API

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const app = express();
+const request = require('request');
+const moment = require('moment');
 
-app.get('/', (req, res) => {
-  res.end('hi');
+app.get('/api', (req, res) => {
+  var { stop } = req.query;
+
+  request(
+    {
+      uri: 'http://www.pittshuttle.com/Services/JSONPRelay.svc/GetMapStopEstimates?TimesPerStopString=8&ApiKey=8882812681',
+      json: true
+    },
+    (err, response, body) => {
+      if (err) throw err;
+
+      res.json({
+        stopName: stop,
+        arrivals: body.reduce((arr, route) => {
+          var stops = route.RouteStops.filter((routeStop) => {
+            return routeStop.Description.includes(stop);
+          });
+
+          if (stops.length > 0) {
+            arr.push({
+              route: route.Description.trim(),
+              arrivals: stops[0].Estimates.map((estimate) => {
+                return moment()
+                .add({ seconds: estimate.SecondsToStop })
+                .diff(moment(), 'minutes');
+              }).sort()
+            });
+          }
+
+          return arr;
+        }, [])
+      });
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
